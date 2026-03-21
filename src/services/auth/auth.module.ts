827a20072c9ea761b2d7b8b4d78d4450fc355edb
Diego-Zarate18 <diegoazarate110800@gmail.com>
2026-03-21 12:53:43 -0600

    implement Prisma integration with PostgreSQL, add user and workspace models, and set up authentication service

---
+++ src/services/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { PrismaModule } from "src/prisma/prisma.module";
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true}),
+        PrismaModule,
+        AuthModule,
+    ],
+})
+export class AppModule {}
